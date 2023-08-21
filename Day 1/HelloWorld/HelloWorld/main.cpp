45fe1cbd9ca37b0d90462fde9327b156f50c3f02
aiden <u1458057@umail.utah.edu>
2023-08-21 11:30:59 -0600

    day 1 lab helloworld

---
+++ Day 1/HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Aiden Pratt on 8/21/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    //adding cout of 2+2
+    std::cout << "2 + 2 = 4\n";
+    //original Hello, World! line
+    std::cout << "Hello, World!\n";
+    return 0;
+   
+}
