b60745520b51cddb936d51571aa44ff319240a0b
aiden <u1458057@umail.utah.edu>
2023-08-26 19:40:31 -0600

    adding Day5

---
+++ Day5/FunctionPractice/FunctionPractice/main.cpp
@@ -0,0 +1,137 @@
+//
+//  main.cpp
+//  FunctionPractice
+//
+//  Created by Aiden Pratt on 8/25/23.
+//
+
+#include <iostream>
+#include <cmath>
+
+//part 1 (a) function to find the hyoptenuse of inputted numbers
+float findHypotenuse(float a, float b){
+    float hypotenuseAns = sqrt((pow(a , 2)) + (pow(b, 2)));
+    return hypotenuseAns;
+}
+
+//part 1 (b) a function to determine x and y velocity
+void findVelocity(double x, double y){
+    double veloictyAnsX = x*cos(y);
+    double velocityAnsY = x*sin(y);
+    
+    std::cout << veloictyAnsX << " " << velocityAnsY << "\n";
+    
+}
+//part 2 isCapitalized
+bool isCapitalized(std::string inputS){
+    return std::isupper(inputS[0]);
+}
+ 
+
+//bool boolToString()
+void boolToString(bool X){
+    if (X == true){
+        std::cout << "The function is True\n";
+    }
+    else {
+        std::cout << "The function is False\n";
+    }
+    
+}
+
+//isPrime
+bool isPrime(int x){
+    bool numberP = true;
+    
+    if (x == 0 || x == 1){
+        numberP = false;
+    }
+   for (int i = 2; i <= x / 2; i++){
+        if (x % i == 0){
+            numberP = false;
+            break;
+       }
+    }
+        
+        
+    return numberP;
+}
+
+
+
+//MAIN function
+int main(int argc, const char * argv[]) {
+    
+//part 1(a)
+    std::cout << "Enter two sides of your triangle: \n";
+    float inputA = 0;
+    float inputB = 0;
+    std::cin >> inputA;
+    std::cin >> inputB;
+    
+    float hypotenuseAns = findHypotenuse(inputA, inputB);
+    
+    std::cout << "The hyoptenuse is: " << hypotenuseAns << "\n";
+    
+//part 1(b)
+    std::cout << "Enter your speed: \n";
+    double inputSpeed = 0;
+    std:: cout << "Enter your angle: \n";
+    double inputAngle = 0;
+    std::cin >> inputSpeed;
+    std::cin >> inputAngle;
+    
+    findVelocity(inputSpeed, inputAngle);
+    
+//part 1(c) copying code in
+    
+    std::time_t result = std::time(nullptr);
+       std::cout << std::asctime(std::localtime(&result))
+    << result << " seconds since the Epoch\n";
+    //this code checks the local time on the device and then displays the time since Epoch
+    //it is calling "localtime" and putting it into the asctime to display the current date and time since the Epoch
+    
+    
+//part 2
+    //why wouldn't you want input from std::cin inside function?
+    //if you take the cin prompt from the user in the function, they will not be recognized within the main() function because they are seperate.
+    
+    //Why would it be difficult to turn the speed/velocity task above into a function? What imperfect solutions can you come up with that wrap that code into one (or more) functions?
+    //because it is difficult to return more than 2 values. In this case when doing it in a function, we had to just print out the answers of the equation. You could potentially have 2 different functions for each equation to solve this.
+    
+//part 2 isCapitalized
+    std::cout << "Enter a a string: \n";
+    std::string stringCap;
+    std::cin >> stringCap;
+    
+    if (isCapitalized(stringCap)){
+        std::cout << "The first letter is capitalized.\n";
+    }
+    else {
+        std::cout << "The first letter is not capitalized.\n";
+    }
+
+//part 2 boolToString
+    std::cout << "Checking if the function isCapitalized is true: \n";
+    boolToString(isCapitalized(stringCap));
+    
+//part 2 isPrime
+    std::cout << "Enter a number: \n";
+    int primeNum;
+    std::cin >> primeNum;
+    
+    isPrime(primeNum);
+    
+    if (isPrime(primeNum)){
+        std::cout << primeNum <<  " is a prime number. \n";
+    }
+    else {
+        std::cout << primeNum << " is not a prime number. \n";
+    }
+    
+
+    return 0;
+}
+
+
+
---
+++ Day5/StringAnalyzer/StringAnalyzer/main.cpp
@@ -0,0 +1,117 @@
+//
+//  main.cpp
+//  StringAnalyzer
+//
+//  Created by Aiden Pratt on 8/25/23.
+//
+
+#include <iostream>
+#include <string>
+
+
+//WORD COUNT
+double numWords(std::string s){
+    double wordCount = 0;
+    for (int i = 0; i <= s.length(); i++){
+        if (s[i] == ' '){
+            wordCount++;
+        }
+    }
+    return wordCount + 1;
+}
+
+//PUNCTUATION number of sentences
+double numSentences(std::string c){
+    double sentenceCount = 0;
+    for (int i = 0; i < c.length(); i++){
+        if (std::ispunct(c[i])){
+            sentenceCount++;
+        }
+    }
+    return sentenceCount;
+}
+    
+
+//VOWELS
+bool IsVowel(char c){
+    c = tolower(c);
+    return (c=='a'|| c=='e'|| c=='i' || c=='o' || c=='u');
+
+}
+double NumVowels(std::string s){
+    double vowelNum = 0;
+    for (int i = 0; i < s.length(); i++){
+        if (IsVowel(s[i])){
+            vowelNum++;
+
+        }
+    }
+    return vowelNum;
+}
+
+//SPACE
+bool IsSpace( char c){
+    return (c==' ');
+}
+    
+//CONSONANT
+bool IsConsonant(char c){
+    return ( !IsVowel(c) && !ispunct(c) && !IsSpace(c));
+}
+
+double numConsonants(std::string s){
+    double counter = 0;
+    for (int i = 0; i < s.length() ; i++){
+        if (IsConsonant((s[i]))){
+            counter++;
+        }
+    }
+    return counter;
+}
+
+//READING LEVEL
+double AverageWordLength(std::string s){
+    double wordLength = 0.0;
+    wordLength = (NumVowels(s) + numConsonants(s)) / numWords(s);
+        
+    return wordLength;
+    }
+
+//AVG VOWEL PER WORD
+double AverageVowelPerWord(std::string s){
+    double vowelCount = 0.0;
+    vowelCount = (NumVowels(s) / numWords(s));
+    
+    return vowelCount;
+}
+
+//MAIN
+int main(int argc, const char * argv[]) {
+    
+    std::cout << "Enter a string containing one or more sentences: ";
+    std::string input;
+    std::getline( std::cin, input );
+        
+        
+    std::cout << "Number of words: " << numWords(input) << "\n";
+        
+    std::cout << "Number of sentences: "<< numSentences(input) << "\n";
+        
+    std::cout << "Number of vowels: "<< NumVowels(input) << "\n";
+        
+    std::cout << "Number of consonants: " << numConsonants(input) << "\n";
+        
+    std::cout << "Reading level: " << AverageWordLength(input) << "\n";
+        
+    std::cout << "Average vowels per word: " << AverageVowelPerWord(input) << "\n";
+        
+        
+    do {
+    
+    }
+    
+    while (input != "done");
+    
+    
+    return 0;
+}
